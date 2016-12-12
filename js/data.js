a49df77d8885b61ec559c4a8ffb8b302876a2eda
Victor Garcia Zarco <victor.gzarco@gmail.com>
2016-12-12 02:48:32 +0200

    Improved data array: now all the products will be in the same array. Reutilization of data structure in different pages

---
+++ js/data.js
@@ -0,0 +1,36 @@
+var data = {
+	"cdc":[
+		{
+			"iframe":"https://sketchfab.com/models/189d88855a354ab090e99dbc34102945/embed",
+			"text":"First design of CDC",
+			"version":"First design"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/ad823a4ceb1b4c4da7e2c47fb9c398b1/embed",
+			"text":"Second design of CDC",
+			"version":"Second design"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/fb05b3b715cc485b8da6243c39804a18/embed",
+			"text":'To save space people buy and use multifunctional products. We decided to design a new multifunctional product for that aim. The piece can be divided into three different objects, all of which are needed to work in a comfortable space: Chair + Desk + Small table (“coffee table”). With these three objects, you can work anywhere comfortably whilst saving space in your working anviroment or at home for example. The entire piece can be used as storage space. CDC will be useful for students, professionals, designers, co-workers...',
+			"version":"Third design"
+		}
+	],
+	"stb":[
+		{
+			"iframe":"https://sketchfab.com/models/189d88855a354ab090e99dbc34102945/embed",
+			"text":"STB 1",
+			"version":"First design"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/ad823a4ceb1b4c4da7e2c47fb9c398b1/embed",
+			"text":"STB 2",
+			"version":"Second design"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/fb05b3b715cc485b8da6243c39804a18/embed",
+			"text":'STB 3',
+			"version":"Third design"
+		}
+	],
+};
