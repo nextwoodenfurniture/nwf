400794da21ea773bd92501cd6ef88cfb7840cf5e
Victor Garcia Zarco <victor.gzarco@gmail.com>
2016-12-13 18:07:15 +0200

    Added the rest of the products

--- js/data.js
+++ js/data.js
@@ -2,35 +2,74 @@ var data = {
 	"cdc":[
 		{
 			"iframe":"https://sketchfab.com/models/189d88855a354ab090e99dbc34102945/embed",
-			"text":"First design of CDC",
+			"text":"To save space people buy and use multifunctional products. We decided to design a new multifunctional product for that aim. The piece can be divided into three different objects, all of which are needed to work in a comfortable space: Chair + Desk + Small table (“coffee table”). With these three objects, you can work anywhere comfortably whilst saving space in your working anviroment or at home for example. The entire piece can be used as storage space. CDC will be useful for students, professionals, designers, co-workers...",
 			"version":"First design"
 		},
 		{
 			"iframe":"https://sketchfab.com/models/ad823a4ceb1b4c4da7e2c47fb9c398b1/embed",
-			"text":"Second design of CDC",
+			"text":"After testing the fi rst design of this multifunctional product we realised that the product was unstable and the comfortability wasn’t great. We decided to develop a new design changing the shape of CDC. The only thing needed to make the product more stable was to turn it upside down. That was not the only thing that we changed. It’s really difi cult to produce an entire piece of wood bended to be curved, so we decided to use straight lines. After fi xing the problem of stability the design is larger and lighter than before. However, comfortabulity was still an issue and another problem was found regarding the joints of the chair.",
 			"version":"Second design"
 		},
 		{
 			"iframe":"https://sketchfab.com/models/fb05b3b715cc485b8da6243c39804a18/embed",
-			"text":'To save space people buy and use multifunctional products. We decided to design a new multifunctional product for that aim. The piece can be divided into three different objects, all of which are needed to work in a comfortable space: Chair + Desk + Small table (“coffee table”). With these three objects, you can work anywhere comfortably whilst saving space in your working anviroment or at home for example. The entire piece can be used as storage space. CDC will be useful for students, professionals, designers, co-workers...',
+			"text":'To save space people buy and use multifunctional products. We decided to design a new multifunctional product for that aim. The piece can be divided into three different objects, all of which are needed to work in a comfortable space: Chair + Desk + Small table (“coffee table”). With these three objects, you can work anywhere comfortably whilst saving space in your working anviroment or at home for example. The entire piece can be used as storage space. CDC will be useful for students, professionals, designers, co-workers... Due to the comfortability and joint problems we changed the design once more and now we have our final design. We stuck with the straight lines and the shape is like the previous design. However now the design is lighter and more comfortable. We followed a trapezoidal design using the same shape in the bottom and top parts of the product. The problems with the joints were solved by using a wooden frame and fi lling the space in between with plywood, making a high-quality product, cheap, comfortable and appealing. With this design we also changed the use of the coffee table. In the previous versions of this product the coffee table was a large piece which you could use for storage. Our new design consists of two tables which can be used as a stool.',
 			"version":"Third design"
 		}
 	],
 	"stb":[
 		{
-			"iframe":"https://sketchfab.com/models/189d88855a354ab090e99dbc34102945/embed",
-			"text":"STB 1",
-			"version":"First design"
+			"iframe":"https://sketchfab.com/models/1f9d83647af047c6a98117d905298db1/embed",
+			"text":"This product is a combination of three common products that everybody has in their homes. A sofa, a table and a bed. As you will see in the images and drawings , the cushions on the sofa can be used as stools when you use the product as a table. Under the sofa there is plenty of storage space as well as a mattress for the bed. To use the product as a bed the user needs to open the back part of the sofa and move it to locate the whole structure in a stable position.",
+			"version":"Sofa version"
 		},
 		{
-			"iframe":"https://sketchfab.com/models/ad823a4ceb1b4c4da7e2c47fb9c398b1/embed",
-			"text":"STB 2",
-			"version":"Second design"
+			"iframe":"https://sketchfab.com/models/fb6cd9c389634de9a67a49f78af1c3b7/embed",
+			"text":"This product is a combination of three common products that everybody has in their homes. A sofa, a table and a bed. As you will see in the images and drawings , the cushions on the sofa can be used as stools when you use the product as a table. Under the sofa there is plenty of storage space as well as a mattress for the bed. To use the product as a bed the user needs to open the back part of the sofa and move it to locate the whole structure in a stable position.",
+			"version":"Table version"
 		},
 		{
-			"iframe":"https://sketchfab.com/models/fb05b3b715cc485b8da6243c39804a18/embed",
-			"text":'STB 3',
-			"version":"Third design"
+			"iframe":"https://sketchfab.com/models/1f9d83647af047c6a98117d905298db1/embed",
+			"text":'This product is a combination of three common products that everybody has in their homes. A sofa, a table and a bed. As you will see in the images and drawings , the cushions on the sofa can be used as stools when you use the product as a table. Under the sofa there is plenty of storage space as well as a mattress for the bed. To use the product as a bed the user needs to open the back part of the sofa and move it to locate the whole structure in a stable position.',
+			"version":"Bed version"
 		}
+	],
+	"hourglass":[
+		{
+			"iframe":"https://sketchfab.com/models/aaf49266d7f242a1a45541626dcaf89a/embed",
+			"text":"The hourglass line of products is an attempt to renovate some of the lines of products that Puulon sells. Some of their line of products, like “inch” or “boom-sarja”, are composed of chairs, tables and coffee tables. We decided to create this new line of products with the same idea, but adding something that Puulon hasn’t got, such as lamps. One idea we found was “regulated surfaces”. To build the structure for the chairs and tables we wanted to use wooden bars and for the chair and table surfaces we wanted to use plywood. These products are very light and resistant because they are composed by thin wooden bars which are resistant due to the positioning of the structure.",
+			"version":"Chair version"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/228e0fbbaaf24753b9c39a95cacb9917/embed",
+			"text":"The hourglass line of products is an attempt to renovate some of the lines of products that Puulon sells. Some of their line of products, like “inch” or “boom-sarja”, are composed of chairs, tables and coffee tables. We decided to create this new line of products with the same idea, but adding something that Puulon hasn’t got, such as lamps. One idea we found was “regulated surfaces”. To build the structure for the chairs and tables we wanted to use wooden bars and for the chair and table surfaces we wanted to use plywood. These products are very light and resistant because they are composed by thin wooden bars which are resistant due to the positioning of the structure.",
+			"version":"Lamp version"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/59508203efc74ee2909e3510762a1ed4/embed",
+			"text":"The hourglass line of products is an attempt to renovate some of the lines of products that Puulon sells. Some of their line of products, like “inch” or “boom-sarja”, are composed of chairs, tables and coffee tables. We decided to create this new line of products with the same idea, but adding something that Puulon hasn’t got, such as lamps. One idea we found was “regulated surfaces”. To build the structure for the chairs and tables we wanted to use wooden bars and for the chair and table surfaces we wanted to use plywood. These products are very light and resistant because they are composed by thin wooden bars which are resistant due to the positioning of the structure.",
+			"version":"Stool version"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/3948b018e5ea4dedbdb611bf21e3c328/embed",
+			"text":"The hourglass line of products is an attempt to renovate some of the lines of products that Puulon sells. Some of their line of products, like “inch” or “boom-sarja”, are composed of chairs, tables and coffee tables. We decided to create this new line of products with the same idea, but adding something that Puulon hasn’t got, such as lamps. One idea we found was “regulated surfaces”. To build the structure for the chairs and tables we wanted to use wooden bars and for the chair and table surfaces we wanted to use plywood. These products are very light and resistant because they are composed by thin wooden bars which are resistant due to the positioning of the structure.",
+			"version":"Table and chairs version"
+		}
+	],
+	"modularity":[
+		{
+			"iframe":"https://sketchfab.com/models/ac71094775494c0f85deffae481c5954/embed",
+			"text":"We designed a storage piece which can be used for spices, wine bottles, books and so on. The product can be produced on a larger scale and can be attached to the wall for the use of bookshelves for example. The modular pieces have standard measurements for the bottle, spice or books. With different sizes and combinations the user can create his/her own product.",
+			"version":"Bottle rack version"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/1ded61e580734c8788f728a18f8c42df/embed",
+			"text":"We designed a storage piece which can be used for spices, wine bottles, books and so on. The product can be produced on a larger scale and can be attached to the wall for the use of bookshelves for example. The modular pieces have standard measurements for the bottle, spice or books. With different sizes and combinations the user can create his/her own product.",
+			"version":"Bookshelf 1 version"
+		},
+		{
+			"iframe":"https://sketchfab.com/models/4953e4e637574bd3bb623c3b903ab349/embed",
+			"text":"We designed a storage piece which can be used for spices, wine bottles, books and so on. The product can be produced on a larger scale and can be attached to the wall for the use of bookshelves for example. The modular pieces have standard measurements for the bottle, spice or books. With different sizes and combinations the user can create his/her own product.",
+			"version":"Bookshelf 2 version"
+		},
 	],
 };
